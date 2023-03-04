2cf3011b61370b36e3448d073f4d0b0d1078127a
sandervw <sam.vanwilligen@gmail.com>
2023-03-04 14:33:25 -0600

    Worked on adding AI returns for prompts

--- backend/routes/promptRoutes.js
+++ backend/routes/promptRoutes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const Prompt = require('../models/promptModel');
+const ai = require('openai');
+//import {Configuration, OpenAIApi} from 'openai';
 
 const router = express.Router();
 
+//OPENAI configuration and init
+const configuration = new ai.Configuration({
+    //organization: process.env.OPENAI_ORG,
+    apiKey: process.env.OPENAI_KEY
+})
+const openai = new ai.OpenAIApi(configuration);
 
 //get all prompts
 router.get('/', (req, res) => {
@@ -19,13 +27,19 @@ router.post('/', async (req, res) => {
     const {promptType, prompt} = req.body;
     const responseTime = 0;
     //TODO start tracking response time
-    //TODO call GPT API with prompt
-    const response = 'TODO';
-    console.log(promptType, prompt, response, responseTime);
-
     try {
-        const result = await Prompt.create({promptType, prompt, response, responseTime});
-        res.status(200).json(result);
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo", //GPT 3.5 : https://platform.openai.com/docs/models/gpt-3-5
+            messages: [{
+                role: "user",
+                content: prompt
+            }],
+            temperature: 0.9, //used to determine how varied the responses are : 0 is same every time
+            max_tokens: 1000,
+        })
+        console.log(promptType, prompt, responseTime);
+        //const result = await Prompt.create({promptType, prompt, response, responseTime});
+        res.status(200).json(response.data);
     } catch (err) {
         console.log(err);
         res.status(400).json({error: err.message});
